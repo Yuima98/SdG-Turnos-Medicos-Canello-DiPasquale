c693477dc1c17e38629a8c2c56e2ba210d694b70
Agus <agus_d_98@hotmail.com>
2026-08-11 19:23:47 -0300

    setup Express y endpoint /health

---
+++ src/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const pool = require('./database/db');
+
+const app = express();
+app.use(express.json());
+
+app.get('/health', async (req, res) => {
+  try {
+    await pool.query('SELECT 1');
+    res.json({ codigo: 200, estado: 'ok', datos: null });
+  } catch (err) {
+  console.error(err);
+  res.status(500).json({ codigo: 500, estado: 'error de conexión a la base', datos: null });
+}
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
